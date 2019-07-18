f4ae92265d81e9645d197300137bb3e80d07835f
KimblesAndBits <wearethekimbles@gmail.com>
2019-07-18 11:04:00 -0700

    controller done

---
+++ controller/burger_controller.js
@@ -0,0 +1,26 @@
+express = require("express");
+require("../models/burger");
+
+var router = express();
+var PORT = process.env.PORT || 8080
+
+module.exports = function() {
+    router.get('/api/find', (req, res) => {
+        var burgerList = findBurgers();
+        return res.json(burgerList);
+    });
+
+    router.post('/api/update', (req, res) => {
+        var burgerName = req.body;
+        updateBurger(burgerName);
+    });
+
+    router.post('/api/insert', (req, res) => {
+        var burgerName = req.body;
+        insertBurger(burgerName);
+    });
+
+    app.listen(PORT, function() {
+        console.log("App listening on http://localhost" + PORT);
+      });
+}
