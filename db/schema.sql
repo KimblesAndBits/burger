ad9efe138717319023b063ed2fe6714ba18a70e4
KimblesAndBits <wearethekimbles@gmail.com>
2019-07-18 10:26:24 -0700

    initial file additions. connection to mysql written

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+CREATE TABLE `burger` (
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`burger_name` VARCHAR(140) NOT NULL,
+	`devoured` BOOLEAN DEFAULT false,
+	PRIMARY KEY (id));
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burger_db;
+INSERT INTO burger (burger_name)
+VALUES ("Bacon Burger"), ("Peanut Butter Banana Burger"), ("Hawaiian Burger with Pineapple");
